172a15a5c22c26b4d73834406508f5c8cba031d0
Joe <141609881+uarefire@users.noreply.github.com>
2023-09-06 03:10:24 +0000

    Update MemoryDump_dmp.py

--- Windows/MemoryDump_dmp.py
+++ Windows/MemoryDump_dmp.py
@@ -1,22 +1,25 @@
-import shutil
 import os
-
-# 원본 Memory.dmp 파일 경로
-source_path = os.path.join(os.environ['SystemRoot'], 'Memory.dmp')
-
+import shutil
+# 검색을 시작할 디렉토리 설정 (루트 디렉토리부터 검색하려면 'C:\\' 또는 다른 드라이브 경로로 설정)
+search_directory = 'C:\\'
+# 검색 대상 파일 이름
+file_to_find = 'MEMORY.DMP'
+# 검색 및 복사 작업
+found = False  # 파일을 찾았는지 여부를 나타내는 플래그
 # 사용자의 바탕화면 경로
 desktop_path = os.path.join(os.environ['USERPROFILE'], 'Desktop')
-
 # 복사될 Memory.dmp 파일 경로
 destination_path = os.path.join(desktop_path, 'Memory.dmp')
-
-# Memory.dmp 파일을 바탕화면으로 복사
-try:
-    shutil.copy(source_path, destination_path)
-    print(f"{source_path}를 {destination_path}로 복사 완료.")
-except PermissionError:
-    print("관리자 권한이 필요합니다.")
-except FileNotFoundError:
-    print("원본 파일을 찾을 수 없습니다.")
-except Exception as e:
-    print(f"파일 복사 중 오류 발생: {e}")
+for foldername, subfolders, filenames in os.walk(search_directory):
+    if file_to_find in filenames:
+        found = True
+        source_path = os.path.join(foldername, file_to_find)
+        try:
+            shutil.copy(source_path, destination_path)
+            print(f"{file_to_find}를 {destination_path}로 복사 완료.")
+        except PermissionError:
+            print("관리자 권한이 필요합니다.")
+        except Exception as e:
+            print(f"파일 복사 중 오류 발생: {e}")
+if not found:
+    print(f"{file_to_find} 파일을 찾을 수 없습니다.")
